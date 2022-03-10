1fa89515b25aefb05612a6f080293053b4c81a6d
umerfareed72 <60812809+umerfareed72@users.noreply.github.com>
2022-03-10 20:10:04 +0500

    Auth design completed

--- src/screens/Splash/Splash.js
+++ src/screens/Splash/Splash.js
@@ -6,8 +6,7 @@ import {appLogos} from '../../shared/theme/assets';
 const Splash = ({navigation}) => {
   useEffect(() => {
     setTimeout(() => {
-      // navigation.replace('GettingStarted');
-      navigation.replace('App');
+      navigation.replace('GettingStarted');
     }, 2500);
   }, []);
 
